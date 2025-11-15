27db40634b703833fd9a19b6a0781ce41baa5776
Eduardo Carvalho <educarvalho009@gmail.com>
2025-11-15 16:29:32 -0400

    feat: generate invoice

--- apps/api/src/services/lnurl.js
+++ apps/api/src/services/lnurl.js
@@ -1,5 +1,5 @@
 // address, i.e. "user@domain.com"
-function addressToUrl(address) {
+async function addressToUrl(address) {
   const regex = /^([^@]+)@([^@]+)$/
   const addressParts = address.match(regex)
 
@@ -9,6 +9,27 @@ function addressToUrl(address) {
   return `https://${domain}/.well-known/lnurlp/${username}`
 }
 
+// url: https://${domain}/.well-known/lnurlp/${username}
+async function generatePaymentRequest(url) {
+  const amountMilliSatoshi = 10000
 
+  const validURL = new URL(url)
+  const LNURLPay = await fetch(validURL)
+  const LNURLPayJSON = await LNURLPay.json()
 
-console.log(addressToUrl("ducarv@bipa.app"))
+  const urlWithAmount = new URL(`${LNURLPayJSON.callback}?amount=${amountMilliSatoshi}`)
+  const callbackUrl = urlWithAmount.href
+  const response = await fetch(callbackUrl)
+  
+  const paymentResquest = await response.json()
+  
+  return paymentResquest.pr
+}
+
+async function main() {
+  const url = await addressToUrl("ducarv@bipa.app");
+  const pr = await generatePaymentRequest(url);
+  console.log(pr);
+}
+
+main();
